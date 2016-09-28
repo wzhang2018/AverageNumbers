4db9a4290fe876d1d45fa76ff571c7143275fdef
Will Zhang <wzhang2018@pingry.org>
2016-09-28 13:00:12 -0400

    Changed the program to ask user for numbers.

--- AverageNumbers.java
+++ AverageNumbers.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
+
 public class AverageNumbers
 {
 	public static void main(String[] args)
 	{
 		// Make an int
-		int[] numbers = {0, -5, 3, 9, 8, 2};
+		int[] numbers = new int[6]; //Once created, size can't change.  Declares size 6, does not initialize.
+
+
+		// Make Scanner
+		Scanner kb =  new Scanner(System.in);
+
+		String input;
+		int userNumber;
+
+		// Now ask the user for the numbers
+		for (int x = 0; x < numbers.length; x++)
+		{
+			System.out.println("Tell me the next number to average.");
+			
+			input = kb.nextLine();
+			userNumber = Integer.parseInt(input);
+
+			numbers[x] = userNumber;
+
+			// number[x] = Integer.parseInt(kb.nextLine());
+		}
 
 		// Add the whole array together
 		int total = 0;
--- README.md
+++ README.md
@@ -2,7 +2,17 @@ Overview
 ========
 
 This program averages an array of integers, and prints the average to the scrreen.
+:+1: :sparkles: :camel: :tada: :rocket: :metal: :octocat:
 
 Usage
 ======
-Compile with: `javac AVerageNumbers.java'
+Compile with: `javac AverageNumbers.java'
+Run with: `java AverageNumbers'
+
+Author
+=======
+AP Computer Science Class
+
+License
+=======
+Open Source
